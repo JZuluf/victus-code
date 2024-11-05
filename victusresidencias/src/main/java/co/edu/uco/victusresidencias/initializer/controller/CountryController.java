5ae899776e041ccfc4a2c4e305d8fabdf998c4a9
Andres Velez <andresjobpk@gmail.com>
2024-11-04 23:33:17 -0500

    Creando Clase controlador para luego hacer pruebas

--- victusresidencias/src/main/java/co/edu/uco/victusresidencias/initializer/controller/CountryController.java
+++ victusresidencias/src/main/java/co/edu/uco/victusresidencias/initializer/controller/CountryController.java
@@ -1,50 +1,62 @@
 package co.edu.uco.victusresidencias.initializer.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import co.edu.uco.victusresidencias.data.dao.impl.postgresql.PostgreSqlDAOFactory;
 import co.edu.uco.victusresidencias.entity.CountryEntity;
-import co.edu.uco.victusresidencias.initializer.domain.City;
-import co.edu.uco.victusresidencias.initializer.repository.CityRepository;
+import co.edu.uco.victusresidencias.crosscutting.helpers.UUIDHelper;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/countries")
 public class CountryController {
-	private PostgreSqlDAOFactory repository;
-	
-	
-	@Autowired
-	public CountryController(PostgreSqlDAOFactory repository) {
-		this.repository = repository;
-	}
 
+    private final PostgreSqlDAOFactory daoFactory;
 
-	@GetMapping("/all")
-	public List<CountryEntity> getAll(){
-		//repository.commitTransaction()
-		return null;//this.PostgreSqlDAOFactory;
-	}
-	
-	@GetMapping("/{id}")
-	public City getById(@PathVariable long id) {
-		return null;//this.cityRepository.getById(id);
-	}
-	
-	@PostMapping("/new")
-	public City saveCity(@RequestBody City city){
-		
-		return null;//this.cityRepository.save(city);
-	}
-	
-	@DeleteMapping("/{id}")
-	public void deleteCity(@PathVariable long id){
-		
-		//this.cityRepository.delete(id);
-	}
+    @Autowired
+    public CountryController() {
+        this.daoFactory = new PostgreSqlDAOFactory();
+    }
 
+    @GetMapping("/all")
+    public List<CountryEntity> findAll() {
+        // Llamamos a findAll del DAO
+        return daoFactory.getCountryDAO().findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CountryEntity findById(@PathVariable UUID id) {
+        // Llamamos a findById del DAO
+        return daoFactory.getCountryDAO().fingByID(id);
+    }
+
+    @PostMapping("/new")
+    public String createCountry(@RequestBody CountryEntity countryEntity) {
+        // Asignar un nuevo UUID si es necesario
+        if (UUIDHelper.isDefault(countryEntity.getId())) {
+            countryEntity.setId(UUID.randomUUID());
+        }
+        
+        // Crear el país usando el DAO
+        daoFactory.getCountryDAO().create(countryEntity);
+        return "Country created successfully with name: " + countryEntity.getName();
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteCountry(@PathVariable UUID id) {
+        // Eliminar país usando el DAO
+        daoFactory.getCountryDAO().delete(id);
+        return "Country deleted successfully with id: " + id;
+    }
+
+    @PutMapping("/{id}")
+    public String updateCountry(@PathVariable UUID id, @RequestBody CountryEntity countryEntity) {
+        // Actualizar datos del país
+        countryEntity.setId(id);
+        daoFactory.getCountryDAO().update(countryEntity);
+        return "Country updated successfully with id: " + id;
+    }
 }
