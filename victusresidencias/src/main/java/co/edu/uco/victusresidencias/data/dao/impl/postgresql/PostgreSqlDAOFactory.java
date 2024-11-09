e0c33c9034da2190b1c60e75d488498feab079d7
JZuluf <148837724+JZuluf@users.noreply.github.com>
2024-11-08 20:34:03 -0500

    Implementación de hikari

--- victusresidencias/src/main/java/co/edu/uco/victusresidencias/data/dao/impl/postgresql/PostgreSqlDAOFactory.java
+++ victusresidencias/src/main/java/co/edu/uco/victusresidencias/data/dao/impl/postgresql/PostgreSqlDAOFactory.java
@@ -20,12 +20,12 @@ import co.edu.uco.victusresidencias.data.dao.UsageTimeUnitDAO;
 public final class PostgreSqlDAOFactory extends DAOFactory {
 
 	private Connection connection;
-	private final String url = "jdbc:postgresql://localhost:5432/LocalBaseDatosJava";
-	private final String user = "postgres";
-	private final String password = "123456";
-//	private final String url = "jdbc:postgresql://bubble.db.elephantsql.com/cjjxjjym";
-//	private final String user = "cjjxjjym";
-//	private final String password = "C1fGqQx4sC0Jh6Rg3TicDI1WJ1UxAUJk";
+//	private final String url = "jdbc:postgresql://localhost:5432/LocalBaseDatosJava";
+//	private final String user = "postgres";
+//	private final String password = "123456";
+	private final String url = "jdbc:postgresql://bubble.db.elephantsql.com/cjjxjjym";
+	private final String user = "cjjxjjym";
+	private final String password = "C1fGqQx4sC0Jh6Rg3TicDI1WJ1UxAUJk";
 	
 
 	public PostgreSqlDAOFactory() {
